9159320d0b7bb863522237c3767ca7677d38362f
Lu Li <lilu.bupt.cn@gmail.com>
2016-03-11 00:30:28 -0800

    before array, 13:51

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Go Language Basic
+# start code for learning go language
---
+++ src/hello/hello.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var age int = 40
+	var num float64 = 1.618
+	var pi float64 = 3.1415926
+	fmt.Println(age, num)
+	fmt.Printf("%d \n", 100) // integer
+	fmt.Printf("%b \n", 100) // binary
+	fmt.Printf("%c \n", 44)  // character
+	fmt.Printf("%x \n", 17)  // hex
+	fmt.Printf("%e \n", pi)  // scientific type
+	fmt.Printf("hello, world\n")
+}
